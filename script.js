1eca6ef50d07f8705b2161a834ee6c16829de397
temcray <ejamison06@gmail.com>
2025-06-27 17:02:34 -0500

    pets salon

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log("Pet Salon project ")
+
+
+const pets = ["Max", "Michelle", "Bellia"];
+document.getElementById("name").innerHTML = petsName;
+
+
+const pet1 = {}
+
+person.firstName = "Max";
+person.lastName = "Million";
+person.age = 2;
+person.breed = "dog";
+document.getElementById("name").innerHTML =
+    person.firstName + " Max " + person.age + " two year old.";
+
+
+const pet2 = {};
+person.firstName = "Michelle";
+person.lastName = "Jamison";
+person.age = 7;
+person.breed = "cat";
+document.getElementById("name").innerHTML =
+    person.firstName + " Michelle " + person.age + " seven year old.";
+
+const pet3 = {};
+person.firstName = "Bellia";
+person.lastName = "Young";
+person.age = 4;
+person.breed = "dog";
+document.getElementById("name").innerHTML =
+    person.firstName + " Bellia " + person.age + " four months old.";

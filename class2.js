0b63db1cb9225d89f643f2c74ec3d3c67e0fae5f
Tatiana McCray <ejamison06@gmail.com>
2025-06-30 17:15:45 -0500

    Add files via upload

---
+++ class2.js
@@ -0,0 +1,28 @@
+//Create an Object
+const movies = ["cadillac records", "wedding crashers", "merry little christmas"];
+
+console.log("movies title")
+document.getElementById("demo").innerHTML = movies;
+
+//array num
+let number = [2,3,6,9,7];
+
+
+
+
+
+
+//ex2
+let book = {
+    name:"yes please!",
+    author:"Amy..",
+    year: 2000
+}
+console.log(`the book name is: ${book.nmae} and the year is ${book.year} `);
+
+let user = {
+    userName:"Kelly, Rowland",
+    eMail:"keldc1997@gmail.com",
+    age:44,
+}
+console.log(`Hello ${"Kelly,Rowland"}! ${"keldc1997@gmail.com"},${45}`);

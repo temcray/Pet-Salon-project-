45f33d50ec8adddf4f8dda9557e97b35d0c2c807
Tatiana McCray <ejamison06@gmail.com>
2025-07-04 12:04:15 -0500

    Add files via upload

---
+++ FORLOOPS.js
@@ -0,0 +1,11 @@
+console.log("for loop");
+
+for (let i = 0; i <= 5; i++) {
+    console.log("Tatiana you rock");
+    document.getElementById("result").innerHTML += "Tatiana you rock";
+
+}
+
+for( let i = 2; i < 10; i--){
+    console.log("Beyonce");
+}
